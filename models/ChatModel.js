a16a49082bf8dd2c695f4cb6a8c51f92e33ce197
Abdul Fatah <fatah03111995@gmail.com>
2024-08-15 09:59:29 +0700

    feat : Chat Model

---
+++ models/ChatModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const ChatSchema = mongoose.Schema(
+  {
+    from: {
+      type: String,
+      required: true,
+      max: 30,
+    },
+    to: {
+      type: String,
+      required: true,
+      max: 30,
+    },
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ChatModel = mongoose.model('chat', ChatSchema);
+export default ChatModel;
